5ab876fa0713620f9fef0b451b1c7366cc7b28cd
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-08-28 07:25:44 +0800

    移除對 Sentry 的相依

--- config/wsgi.py
+++ config/wsgi.py
@@ -17,7 +17,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
@@ -33,7 +32,6 @@ application = get_wsgi_application()
 # Use Whitenoise to serve static files
 # See: https://whitenoise.readthedocs.org/
 application = DjangoWhiteNoise(application)
-application = Sentry(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
