1b70bdb581ec90b3a852de8e121602e67812c7f5
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-09-04 04:45:59 +0800

    修正 ELO 的 admin 選單，新增 author 欄位

--- commonrepo/elos/admin.py
+++ commonrepo/elos/admin.py
@@ -7,7 +7,7 @@ from .models import ELO
 
 class ELOAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['name', 'fullname']}),
+        (None,               {'fields': ['name', 'fullname', 'author']}),
         ('Date information', {'fields': ['create_date', 'update_date']}),
         ('ELO Type',         {'fields': ['original_type']}),
     ]
