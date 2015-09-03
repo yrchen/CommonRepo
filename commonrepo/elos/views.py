54f317ac6d7f8118e9e766028ae75c474786140e
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-09-04 04:45:32 +0800

    新增 My ELO List Viewer

--- commonrepo/elos/views.py
+++ commonrepo/elos/views.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 from django.shortcuts import render
 
-# Create your views here.
+from braces.views import LoginRequiredMixin
+from .models import ELO
+
+class MyELOsListView(LoginRequiredMixin, ListView):
+    context_object_name = 'myelos_list'
+    template_name = 'elos/elos_user_list.html'
+    
+    def get_queryset(self):
+        return ELO.objects.filter(author=self.request.user)    
