ff1ae5b804888d9a4b49fd255f8ed0a41f1ba2f9
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-09-04 00:40:03 +0800

    修正 ELO 資料庫欄位定義，另外加上 admin 介面選單分類

--- commonrepo/elos/models.py
+++ commonrepo/elos/models.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
-
+@python_2_unicode_compatible
 class ELO(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(_("Name of ELO"), blank=False, max_length=255)
+    fullname = models.CharField(_("Full Name of ELO"), blank=True, max_length=255)
     author = models.CharField(max_length=128)
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField('date created')
+    update_date = models.DateTimeField('date updated')
     original_type = models.SmallIntegerField()
     
-    def __unicode__(self):
+    def __str__(self):
         return self.name
