b59a523ed112bb2828ef28f3bcd847fb0cb4bd12
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-09-04 07:06:24 +0800

    修正 ELO List Viewer

--- commonrepo/elos/views.py
+++ commonrepo/elos/views.py
@@ -9,8 +9,7 @@ from braces.views import LoginRequiredMixin
 from .models import ELO
 
 class MyELOsListView(LoginRequiredMixin, ListView):
-    context_object_name = 'myelos_list'
-    template_name = 'elos/elos_user_list.html'
+    template_name = 'elos/elos_list.html'
     
     def get_queryset(self):
         return ELO.objects.filter(author=self.request.user)    
