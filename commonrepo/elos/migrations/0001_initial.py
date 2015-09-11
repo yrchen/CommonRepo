c1945b6c5d5a90fb7d5fb32f07647c5754973c26
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-09-11 22:50:18 +0800

    刪除所有 ELO 相關的 migrate 程序，避免轉型失敗的問題

--- commonrepo/elos/migrations/0001_initial.py
+++ commonrepo/elos/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,11 +16,20 @@ class Migration(migrations.Migration):
             name='ELO',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=256)),
-                ('author', models.CharField(max_length=128)),
-                ('create_date', models.DateTimeField(auto_now_add=True)),
-                ('update_date', models.DateTimeField(auto_now=True)),
+                ('name', models.CharField(max_length=255, verbose_name='Name of ELO')),
+                ('fullname', models.CharField(max_length=255, verbose_name='Full Name of ELO', blank=True)),
+                ('create_date', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+                ('update_date', models.DateTimeField(auto_now=True, verbose_name='date updated')),
                 ('original_type', models.SmallIntegerField()),
+                ('author', models.ForeignKey(related_name='elos', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ELOType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='Name of ELO type')),
+                ('type_id', models.SmallIntegerField()),
             ],
         ),
     ]
