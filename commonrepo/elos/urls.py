9a36ad6760ed789085b03fb9cc0cf5684b79f58e
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-25 19:30:36 +0800

    更新 ELOs 的 URL 設定，重新排序所有參照順序

--- commonrepo/elos/urls.py
+++ commonrepo/elos/urls.py
@@ -8,26 +8,6 @@ from . import views
 urlpatterns = [
     # URL pattern for the UserListView
     url(
-        regex=r'^create/$',
-        view=views.ELOsCreateView.as_view(),
-        name="elos-create"
-    ),
-    url(
-        regex=r'^fork/(?P<pk>[0-9]+)/$',
-        view=views.ELOsForkView.as_view(),
-        name="elos-fork"
-        ),
-    url(
-        regex=r'^netork/(?P<pk>[0-9]+)/$',
-        view=views.ELOsNetworkView.as_view(),
-        name="elos-network"
-        ),    
-    url(
-        regex=r'^all/$',
-        view=views.ELOsListView.as_view(),
-        name="elos-alllist"
-        ),    
-    url(
         regex=r'^$',
         view=views.ELOsMyListView.as_view(),
         name='elos-mylist'
@@ -36,6 +16,26 @@ urlpatterns = [
         regex=r'^(?P<pk>[0-9]+)/$',
         view=views.ELOsDetailView.as_view(),
         name='elos-detail'
+    ),    
+    url(
+        regex=r'^all/$',
+        view=views.ELOsListView.as_view(),
+        name="elos-alllist"
+    ),    
+    url(
+        regex=r'^create/$',
+        view=views.ELOsCreateView.as_view(),
+        name="elos-create"
+    ),
+    url(
+        regex=r'^fork/(?P<pk>[0-9]+)/$',
+        view=views.ELOsForkView.as_view(),
+        name="elos-fork"
+    ),
+    url(
+        regex=r'^netork/(?P<pk>[0-9]+)/$',
+        view=views.ELOsNetworkView.as_view(),
+        name="elos-network"
     ),
     url(
         regex=r'^update/(?P<pk>[0-9]+)/$',
