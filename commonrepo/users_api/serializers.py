ecc35487ca23a97b9a271e09c447faad2e43bf51
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-27 19:19:21 +0800

    更新 API Users, 提供取得 Users 詳細資訊的方法

--- commonrepo/users_api/serializers.py
+++ commonrepo/users_api/serializers.py
@@ -7,11 +7,11 @@ from commonrepo.users.models import User as User
 
 from commonrepo.elos.models import ELO
 from commonrepo.snippets_api.models import Snippet
-                  
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     elos = serializers.HyperlinkedRelatedField(queryset=ELO.objects.all(), view_name='elos:elos-detail', many=True)
     snippets = serializers.HyperlinkedRelatedField(queryset=Snippet.objects.all(), view_name='snippet-detail', many=True)
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'elos', 'snippets', )
+        fields = ('url', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elos', 'snippets', )
