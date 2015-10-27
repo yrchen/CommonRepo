1d2068b3b972c6105aebdf3c8f5f1df5b2500a10
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-28 01:29:58 +0800

    更新 API Users, 新增一個欄位 elos_published 回傳使用者有多少已經發布的 ELO

--- commonrepo/users_api/serializers.py
+++ commonrepo/users_api/serializers.py
@@ -10,8 +10,18 @@ from commonrepo.snippets_api.models import Snippet
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     elos = serializers.HyperlinkedRelatedField(queryset=ELO.objects.all(), view_name='elos:elos-detail', many=True)
+    elos_published = serializers.SerializerMethodField()
     snippets = serializers.HyperlinkedRelatedField(queryset=Snippet.objects.all(), view_name='snippet-detail', many=True)
+    
+    def get_elos_published(self, obj):
+        user = None
+        request = self.context.get("request")
+        
+        if request and hasattr(request, "user"):
+            user = request.user
+        
+        return ELO.objects.filter(author=user).filter(is_public=1).count()   
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elos', 'snippets', )
+        fields = ('url', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elos', 'elos_published', 'snippets', )
