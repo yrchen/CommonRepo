dbf0107cfe2bdb0191ac94437e39549f72e7e715
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-28 02:06:02 +0800

    更新 API Users, 新增一個欄位 elos_from_others 回傳使用者已經發布的 ELO 中，有多少是來自別人的

--- commonrepo/users_api/serializers.py
+++ commonrepo/users_api/serializers.py
@@ -11,6 +11,7 @@ from commonrepo.snippets_api.models import Snippet
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     elos = serializers.HyperlinkedRelatedField(queryset=ELO.objects.all(), view_name='elos:elos-detail', many=True)
     elos_published = serializers.SerializerMethodField()
+    elos_from_others = serializers.SerializerMethodField()
     snippets = serializers.HyperlinkedRelatedField(queryset=Snippet.objects.all(), view_name='snippet-detail', many=True)
     
     def get_elos_published(self, obj):
@@ -20,8 +21,28 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         if request and hasattr(request, "user"):
             user = request.user
         
-        return ELO.objects.filter(author=user).filter(is_public=1).count()   
+        return ELO.objects.filter(author=user).filter(is_public=1).count()
+    
+    def get_elos_from_others(self, obj):
+        user = None
+        request = self.context.get("request")
+        
+        if request and hasattr(request, "user"):
+            user = request.user
+        
+        elo_count = ELO.objects.filter(
+                author=user
+            ).filter(
+                is_public=1
+            ).count() - ELO.objects.filter(
+                author=user
+            ).filter(
+                is_public=1
+            ).filter(
+                parent_elo=1
+            ).count()
+        return elo_count
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elos', 'elos_published', 'snippets', )
+        fields = ('url', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elos', 'elos_published', 'elos_from_others', 'snippets', )
