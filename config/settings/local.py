cad6b329741c6b32c971c11f41fb435a2728703b
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-27 18:55:04 +0800

    修改 local 開發環境的設定，修改 REST_FRAMEWORK 中 DEFAULT_RENDERER_CLASSES 的設定，提供 BrowsableAPIRenderer 讓瀏覽器可以直接顯示

--- config/settings/local.py
+++ config/settings/local.py
@@ -60,3 +60,16 @@ INSTALLED_APPS += ('django_extensions', )
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 # Your local stuff: Below this line define 3rd party library settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
