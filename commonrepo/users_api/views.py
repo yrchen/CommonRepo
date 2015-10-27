5595609844a4909b05c09e219dbe49db77193f0d
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-27 20:14:56 +0800

    更新 API Users, 允許管理者變更所有使用者資訊

--- commonrepo/users_api/views.py
+++ commonrepo/users_api/views.py
@@ -27,4 +27,5 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAdminUser,
+                          IsOwnerOrReadOnly]
