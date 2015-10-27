565fa5446ce84de95b929bbc6828c276a242ae37
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-10-27 22:47:36 +0800

    暫時關閉 API Users 的權限檢查

--- commonrepo/users_api/views.py
+++ commonrepo/users_api/views.py
@@ -27,5 +27,5 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser,
-                          IsOwnerOrReadOnly]
+    #permission_classes = [permissions.IsAdminUser,
+    #                      IsOwnerOrReadOnly]
