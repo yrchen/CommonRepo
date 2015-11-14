a37dc575d04e0b87e1a1627a4d6cc67d6302b531
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-11-14 20:09:10 +0800

    移除 API Groups 中無用之程式碼

--- commonrepo/groups_api/serializers.py
+++ commonrepo/groups_api/serializers.py
@@ -6,8 +6,6 @@ from rest_framework import serializers
 from commonrepo.groups.models import Group
 from commonrepo.users.models import User as User
 
-from .models import GroupFileUpload
-
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
