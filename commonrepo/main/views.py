8a60a6eecf50a40ae70fd0125617fa0e3746dc6f
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-11-17 01:19:42 +0800

    新增 View Dashboard, 用來顯示一般資訊

--- commonrepo/main/views.py
+++ commonrepo/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from commonrepo.elos.models import ELO
+from commonrepo.users.models import User as User
+
+class DashboardView(TemplateView):
+    
+    template_name='main/dashboard.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['elos_total_count'] = ELO.objects.all().count()
+        context['elos_my_total_count'] = ELO.objects.filter(author=self.request.user).count()
+        context['users_total_count'] = User.objects.all().count()
+        return context
