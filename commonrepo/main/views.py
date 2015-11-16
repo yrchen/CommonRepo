6952f81c188cf71395be9f9e44c78d5dd481186b
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-11-17 01:38:19 +0800

    更新 View Dashboard, 檢查使用者是否登入，若未登入則轉向至 home 頁面

--- commonrepo/main/views.py
+++ commonrepo/main/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
 
 from commonrepo.elos.models import ELO
@@ -11,8 +11,15 @@ class DashboardView(TemplateView):
     
     template_name='main/dashboard.html'
     
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return redirect('home')
+
+        return super(DashboardView, self).dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
+
         context['elos_total_count'] = ELO.objects.all().count()
         context['elos_my_total_count'] = ELO.objects.filter(author=self.request.user).count()
         context['users_total_count'] = User.objects.all().count()
