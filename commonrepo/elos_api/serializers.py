62f0211d8e575e8b3848aede18fd8c903bb9055c
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-11-27 00:15:32 +0800

    更新 API ELO v2, 新增 metadata 欄位

--- commonrepo/elos_api/serializers.py
+++ commonrepo/elos_api/serializers.py
@@ -16,7 +16,7 @@ class ELOSerializer(serializers.HyperlinkedModelSerializer):
 class ELOSerializerV2(serializers.ModelSerializer):
     class Meta:
         model = ELO
-        fields = ('url', 'id', 'name', 'fullname', 'author', 'create_date', 'update_date', 'original_type', 'is_public', 'init_file', 'version', 'parent_elo', 'parent_elo_version' )
+        fields = ('url', 'id', 'name', 'fullname', 'author', 'create_date', 'update_date', 'metadata','original_type', 'is_public', 'init_file', 'version', 'parent_elo', 'parent_elo_version' )
 
 class ELOTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
