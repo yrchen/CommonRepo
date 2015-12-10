df9c2a5c0e470e7f466f166f4e255ebd788bcdc3
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-12-10 20:47:47 +0800

    更新 API Users, 新增回傳使用者 elo_similarity_threshold 的設定

--- commonrepo/users_api/serializers.py
+++ commonrepo/users_api/serializers.py
@@ -63,7 +63,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elos', 'elos_published', 'elos_forks', 'elos_from_others', 'commonrepo_groups', 'commonrepo_groups_members', 'snippets', )
+        fields = ('url', 'id', 'username', 'organization', 'phone', 'address', 'language', 'area', 'teaching_category', 'teaching_subject_area', 'elo_similarity_threshold', 'elos', 'elos_published', 'elos_forks', 'elos_from_others', 'commonrepo_groups', 'commonrepo_groups_members', 'snippets', )
 
 class UserSerializerV2(serializers.ModelSerializer):
     # ELOs information
