14ee6feabac6f512cc64de948c5562c408aab86d
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-12-24 06:18:58 +0800

    更新 config/urls.py, 新增 django-notifications-hq

--- config/urls.py
+++ config/urls.py
@@ -50,6 +50,9 @@ urlpatterns = [
 
     # Message
     url(r'^messages/', include('messages_extends.urls')), # django-messages-extends
+    
+    # Notifications
+    url(r'^notifications-api/', include('notifications.urls', namespace='notifications')),
 
     # Your stuff: custom urls includes go here
     url(r'^elos/', include("commonrepo.elos.urls", namespace="elos")),
