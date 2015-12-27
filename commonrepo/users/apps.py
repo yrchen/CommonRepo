13e2c2aee5ef68c345705e27d1219334d0049918
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-12-28 05:12:44 +0800

    更新 apps Users, 新增 UsersAppConfig 並指定 default_app_config

---
+++ commonrepo/users/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+from actstream import registry
+
+class UsersAppConfig(AppConfig):
+    name = 'commonrepo.users'
+
+    def ready(self):
+        registry.register(self.get_model('User'))
+        import commonrepo.users.signals
