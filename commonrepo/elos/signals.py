439daa0e6b8a8ab1237972ea3d56e96e1bb3c653
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-12-28 05:07:46 +0800

    更新 signals ELOs, 更新 elo_deleted_handler 以預先取得 ELO 被刪除的 signals

--- commonrepo/elos/signals.py
+++ commonrepo/elos/signals.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_delete, post_save
 
 from actstream import action
 
@@ -9,7 +9,7 @@ from .models import ELO
 
 # ELO has been registered with actstream.registry.register
 
-def elo_deleted_handler(sender, instance, created, **kwargs):
+def elo_deleted_handler(sender, instance, **kwargs):
     action.send(instance, verb='was deleted')
 
 def elo_saved_handler(sender, instance, created, **kwargs):
@@ -18,5 +18,5 @@ def elo_saved_handler(sender, instance, created, **kwargs):
     else:
         action.send(instance, verb='was updated')
 
-post_delete.connect(elo_deleted_handler, sender=ELO)
+pre_delete.connect(elo_deleted_handler, sender=ELO)
 post_save.connect(elo_saved_handler, sender=ELO)
