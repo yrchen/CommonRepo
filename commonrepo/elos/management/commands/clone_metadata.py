3301a2139ecda75331260c235781b27aa8a7d86c
Xaver Y.R. Chen <yrchen@ATCity.org>
2015-12-28 02:41:01 +0800

    新增 commands ELOs, 新增 clone_metadata 命令以便複製指定 ELO 之 metadata

---
+++ commonrepo/elos/management/commands/clone_metadata.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from commonrepo.elos.models import ELO, ELOMetadata
+
+class Command(BaseCommand):
+    help = 'Clone Metadata of ELOs'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--source',
+                            type=int,
+                            help='Build Metadata of specific ELOs')
+        parser.add_argument('--target',
+                            nargs='+',
+                            type=int,
+                            help='Build Metadata of specific ELOs')
+
+    def handle(self, *args, **options):
+        try:
+            elo_source = ELO.objects.get(id=options['source'])
+        except ELO.DoesNotExist:
+            raise CommandError('Source ELOs "%s" does not exist' % elo_source)
+
+        if not elo_source.metadata:
+            raise CommandError('Source Metadata of ELOs "%s" does not exist' % elo_source)
+
+        for target in options['target']:
+            try:
+                elo_target = ELO.objects.get(id=target)
+            except ELO.DoesNotExist:
+                raise CommandError('ELOs "%s" does not exist' % target)
+
+            # Delete original metadata
+            if elo_target.metadata:
+                elo_target.metadata.delete()
+
+            metadata = elo_source.metadata
+            metadata.pk = None
+            metadata.save()
+            elo_target.metadata = metadata
+            elo_target.save()
+
+            self.stdout.write('Successfully clone Metadata to target ELO "%s"' % elo_target.id)
