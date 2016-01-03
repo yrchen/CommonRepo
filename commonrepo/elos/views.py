fd393a0124f36fa0256bd5f5ac00badc5b1ba000
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-01-04 02:00:33 +0800

    更新 view ELOs, 修正 ELOsNetworkView 中的 typo

--- commonrepo/elos/views.py
+++ commonrepo/elos/views.py
@@ -95,7 +95,7 @@ class ELOsNetworkView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(ELOsNetworkView, self).get_context_data(**kwargs)
         # Add in a QuerySet of all the books
-        elo = get_object_or_404t(ELO, id=self.kwargs['pk'])
+        elo = get_object_or_404(ELO, id=self.kwargs['pk'])
         parent_elos = []
 
         if elo.parent_elo_id != settings.ELO_ROOT_ID:
