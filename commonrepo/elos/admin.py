84e364466b3a93739acd043df852d4a97cb6fd69
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-01-05 05:51:20 +0800

    更新 admin ELOs, 新增 license 欄位

--- commonrepo/elos/admin.py
+++ commonrepo/elos/admin.py
@@ -11,7 +11,7 @@ from .models import ELO, ELOType, ELOMetadata, ReusabilityTreeNode, ReusabilityT
 class ELOAdmin(VersionAdmin):
     fieldsets = [
         ('ELO Info',         {'fields': ['name', 'fullname', 'author', 'description', 'uuid']}),
-        ('ELO Metadata',     {'fields': ['original_type', 'metadata']}),
+        ('ELO Metadata',     {'fields': ['original_type', 'metadata', 'license']}),
         ('ELO Version',      {'fields': ['version', 'parent_elo', 'parent_elo_uuid', 'parent_elo_version']}),
     ]
     
