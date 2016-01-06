58b4f7e95ada1c4ba9950ec3ea97ff904f8031a5
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-01-06 17:11:31 +0800

    更新 views main, 新增 follow_user() 用來讓使用者追蹤特定使用者

--- commonrepo/main/views.py
+++ commonrepo/main/views.py
@@ -2,8 +2,13 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
-from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
+
+from actstream import actions
 
 from commonrepo.elos.models import ELO
 from commonrepo.users.models import User as User
@@ -31,3 +36,16 @@ class DashboardView(TemplateView):
         context['elos_all_list'] = ELO.objects.all()[:settings.DASHBOARD_MAX_ELOS_ALL_PER_PAGE]
 
         return context
+
+@login_required
+@csrf_exempt
+def follow_user(request, username):
+    """
+    Creates or deletes the follow relationship between ``request.user`` and the ``user``
+    """
+    user = get_object_or_404(User, username=username)
+
+    actions.follow(request.user, user, actor_only=actor_only)
+    user.userprofile.follower.add(request.user)
+
+    return respond(request, 201)   # CREATED
