f5b8605273b35eca164c56f8f02366ef51dc122e
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-01 17:10:05 +0800

    更新 modules main/views.py, 修正 PEP8 警告

--- commonrepo/main/views.py
+++ commonrepo/main/views.py
@@ -9,9 +9,10 @@ from django.views.generic.base import TemplateView
 from commonrepo.elos.models import ELO
 from commonrepo.users.models import User as User
 
+
 class DashboardView(TemplateView):
 
-    template_name='main/dashboard.html'
+    template_name = 'main/dashboard.html'
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
@@ -27,16 +28,21 @@ class DashboardView(TemplateView):
             context['elos_total_count'] = ELO.objects.all().count()
         # only staff can check all ELOs
         else:
-            context['elos_total_count'] = ELO.objects.filter(is_public=1).count()
-        context['elos_my_total_count'] = ELO.objects.filter(author=self.request.user).count()
+            context['elos_total_count'] = ELO.objects.filter(
+                is_public=1).count()
+        context['elos_my_total_count'] = ELO.objects.filter(
+            author=self.request.user).count()
         context['users_total_count'] = User.objects.all().count()
 
         # ELOs
-        context['elos_my_list'] = ELO.objects.filter(author=self.request.user).order_by('-update_date')[:settings.DASHBOARD_MAX_ELOS_MY_PER_PAGE]
+        context['elos_my_list'] = ELO.objects.filter(author=self.request.user).order_by(
+            '-update_date')[:settings.DASHBOARD_MAX_ELOS_MY_PER_PAGE]
 
         if self.request.user.is_staff:
-            context['elos_all_list'] = ELO.objects.all().order_by('-update_date')[:settings.DASHBOARD_MAX_ELOS_ALL_PER_PAGE]
+            context['elos_all_list'] = ELO.objects.all().order_by(
+                '-update_date')[:settings.DASHBOARD_MAX_ELOS_ALL_PER_PAGE]
         else:
-            context['elos_all_list'] = ELO.objects.filter(is_public=1).order_by('-update_date')[:settings.DASHBOARD_MAX_ELOS_ALL_PER_PAGE]
+            context['elos_all_list'] = ELO.objects.filter(is_public=1).order_by(
+                '-update_date')[:settings.DASHBOARD_MAX_ELOS_ALL_PER_PAGE]
 
         return context
