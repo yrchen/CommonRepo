7e912274060037ee716e179b43cd703d1c55707c
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-01 17:11:55 +0800

    更新 modules users/models.py, 修正 PEP8 警告

--- commonrepo/users/models.py
+++ commonrepo/users/models.py
@@ -9,6 +9,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from annoying.fields import AutoOneToOneField
 
+
 @python_2_unicode_compatible
 class User(AbstractUser):
 
@@ -17,31 +18,44 @@ class User(AbstractUser):
 
     # Basic User Information
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
-    organization = models.CharField(_("Organization/School"), blank=True, max_length=255)
+    organization = models.CharField(
+        _("Organization/School"),
+        blank=True,
+        max_length=255)
     education = models.CharField(_("Education"), blank=True, max_length=255)
     url = models.URLField(_("URL"), blank=True)
     phone = models.CharField(_("Phone nubmer"), blank=True, max_length=255)
     address = models.CharField(_("Address"), blank=True, max_length=255)
-    language =  models.CharField(_("Language"), blank=True, max_length=255)
+    language = models.CharField(_("Language"), blank=True, max_length=255)
     area = models.CharField(_("Area/Nation"), blank=True, max_length=255)
     about = models.CharField(_("About Me"), blank=True, max_length=255)
-    social_facebook = models.URLField(_("Social - Facebook"), blank=True, max_length=255)
-    social_google = models.URLField(_("Social - Google Plus"), blank=True, max_length=255)
-    social_linkedin = models.URLField(_("Social - Linkedin"), blank=True, max_length=255)
-    social_twitter = models.URLField(_("Social - Twitter"), blank=True, max_length=255)
+    social_facebook = models.URLField(
+        _("Social - Facebook"), blank=True, max_length=255)
+    social_google = models.URLField(
+        _("Social - Google Plus"),
+        blank=True,
+        max_length=255)
+    social_linkedin = models.URLField(
+        _("Social - Linkedin"), blank=True, max_length=255)
+    social_twitter = models.URLField(
+        _("Social - Twitter"), blank=True, max_length=255)
 
     # User Pedagogical Information
-    teaching_category = models.CharField(_("Teaching Category"), blank=True, max_length=255)
-    teaching_subject_area = models.CharField(_("Teaching Subject Area"), blank=True, max_length=255)
-    
+    teaching_category = models.CharField(
+        _("Teaching Category"), blank=True, max_length=255)
+    teaching_subject_area = models.CharField(
+        _("Teaching Subject Area"), blank=True, max_length=255)
+
     # Preferences
-    elo_similarity_threshold = models.FloatField(_("ELOs Similarity Threshold"), default=0)
+    elo_similarity_threshold = models.FloatField(
+        _("ELOs Similarity Threshold"), default=0)
 
     def __str__(self):
         return self.username
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
+
 
 @python_2_unicode_compatible
 class UserProfile(models.Model):
