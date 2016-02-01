1fdef0ffdab8d9eac89f36fcb6b7598e2f6050f1
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-01 15:53:45 +0800

    更新 modules elos/apps.py, 修正 PEP8 警告

--- commonrepo/elos/apps.py
+++ commonrepo/elos/apps.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from django.apps import AppConfig
 from actstream import registry
 
+
 class ELOsAppConfig(AppConfig):
     name = 'commonrepo.elos'
 
