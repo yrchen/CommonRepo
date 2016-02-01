886a55cde5b2463878ffc95a91b7f3c6b715fcf3
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-01 17:09:25 +0800

    更新 modules main/signals.py, 修正 PEP8 警告

--- commonrepo/main/signals.py
+++ commonrepo/main/signals.py
@@ -7,8 +7,14 @@ from django_comments.signals import comment_was_posted
 from threadedcomments.models import ThreadedComment
 
 # Comment has been registeded with actstream.registry.register
+
+
 def comment_was_posted_handler(sender, comment, request, **kwargs):
-    action.send(request.user, verb='posted comment', action_object=comment, target=comment.content_object)
+    action.send(
+        request.user,
+        verb='posted comment',
+        action_object=comment,
+        target=comment.content_object)
 
 registry.register(ThreadedComment)
 comment_was_posted.connect(comment_was_posted_handler)
