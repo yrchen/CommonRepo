2dbe8915d3a041ec584356a7735d1f574ac7c8d1
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-03 00:01:34 +0800

    更新 config/production.py, 新增版權宣告資訊

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
+
+#
+# Copyright 2016 edX PDR Lab, National Central University, Taiwan.
+#
+#     http://edxpdrlab.ncu.cc/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Created By: yrchen@ATCity.org
+# Maintained By: yrchen@ATCity.org
+#
+
 '''
-Production Configurations
+Production Configurations for Common Repo projects.
+http://www.commonrepo.com/
 
 - Use djangosecure
 - Use Amazon's S3 for storing static files and uploaded media
@@ -8,7 +31,6 @@ Production Configurations
 - Use Redis on Heroku
 
 - Use sentry for error logging
-
 '''
 from __future__ import absolute_import, unicode_literals
 
