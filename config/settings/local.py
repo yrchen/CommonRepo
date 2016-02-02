8b7171bdaeb77e112c99bb147401576a26a43f1a
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-02 23:58:57 +0800

    更新 config/local.py, 修正 W291

--- config/settings/local.py
+++ config/settings/local.py
@@ -67,7 +67,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ),    
+    ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
