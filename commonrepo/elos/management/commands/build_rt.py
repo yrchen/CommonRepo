dff5c44859d364f384b4899d606450ef88aee7cd
Xaver Y.R. Chen <yrchen@ATCity.org>
2016-02-05 03:03:30 +0800

    更新 modules ELOs 中的 management command 'build_rt', 新增函式功能宣告註解

--- commonrepo/elos/management/commands/build_rt.py
+++ commonrepo/elos/management/commands/build_rt.py
@@ -1,10 +1,39 @@
 # -*- coding: utf-8 -*-
+
+#
+# Copyright 2016 edX PDR Lab, National Central University, Taiwan.
+#
+#     http://edxpdrlab.ncu.cc/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Created By: yrchen@ATCity.org
+# Maintained By: yrchen@ATCity.org
+#
+
+'''
+Management command ``build_rt`` of ELOs in Common Repo projects.
+
+This command will build the Reusabilliy Tree with specefic ELOs.
+'''
+
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 from commonrepo.elos.models import ELO
+
 
 class Command(BaseCommand):
     help = 'Build RT of ELOs'
@@ -34,7 +63,9 @@ class Command(BaseCommand):
 
                 elo.reusability_tree_build()
 
-                self.stdout.write('Successfully builded RT of ELO "%s"' % elo_id)
+                self.stdout.write(
+                    'Successfully builded RT of ELO "%s"' %
+                    elo_id)
 
         if options['all']:
             elos = ELO.objects.all()
@@ -42,6 +73,8 @@ class Command(BaseCommand):
             self.stdout.write('Building RTs of all ELOs...')
             for elo in elos:
                 elo.reusability_tree_build()
-                self.stdout.write('Successfully builded RT of ELO "%s"' % elo_id)
+                self.stdout.write(
+                    'Successfully builded RT of ELO "%s"' %
+                    elo_id)
 
             self.stdout.write('Successfully builded RTs of all ELOs')
